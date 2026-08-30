c3b2a09802130af15fbf97d07519fb2d97a71e06
Nagaraj Lakshatti <nagaraj.lakshatti11@gmail.com>
2026-08-31 02:52:11 +0530

    ooo

--- src/test/java/runners/AllTestsRunner.java
+++ src/test/java/runners/AllTestsRunner.java
@@ -18,7 +18,7 @@ import org.testng.annotations.DataProvider;
 public class AllTestsRunner extends AbstractTestNGCucumberTests {
 
         @Override
-        @DataProvider(parallel = true)
+//        @DataProvider(parallel = true)
         public Object[][] scenarios() {
                 return super.scenarios();
         }
