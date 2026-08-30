bd51e180c830a240b1de3dc84a3a699b8aec8ef2
Nagaraj Lakshatti <nagaraj.lakshatti11@gmail.com>
2026-08-31 03:11:01 +0530

    runner fix

--- src/test/java/runners/AllTestsRunner.java
+++ src/test/java/runners/AllTestsRunner.java
@@ -3,7 +3,6 @@ package runners;
 
 import io.cucumber.testng.CucumberOptions;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
-import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
         features = "src/test/resources/features",
@@ -16,10 +15,4 @@ import org.testng.annotations.DataProvider;
         monochrome = true
 )
 public class AllTestsRunner extends AbstractTestNGCucumberTests {
-
-        @Override
-//        @DataProvider(parallel = true)
-        public Object[][] scenarios() {
-                return super.scenarios();
-        }
 }
--- src/test/java/runners/WebTestRunner.java
+++ src/test/java/runners/WebTestRunner.java
@@ -2,7 +2,6 @@ package runners;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
         features = "src/test/resources/features/web",
@@ -16,10 +15,4 @@ import org.testng.annotations.DataProvider;
 )
 
 public class WebTestRunner extends AbstractTestNGCucumberTests {
-
-    @Override
-//    @DataProvider(parallel = true)
-    public Object[][] scenarios() {
-        return super.scenarios();
-    }
 }
